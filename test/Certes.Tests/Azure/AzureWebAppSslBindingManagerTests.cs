cac8b70b058e4bd674b36b4751f4807df45c7135
Eddie Lin <fszlin@gmail.com>
2016-07-07 19:27:28 -0400

    Add user secrets for testing.

---
+++ test/Certes.Tests/Azure/AzureWebAppSslBindingManagerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Certes.Azure
+{
+    public class AzureWebAppSslBindingManagerTests
+    {
+        [Fact(Skip = "In progress")]
+        public async Task CanGetHostNames()
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets()
+                .AddEnvironmentVariables()
+                .Build();
+
+            var services = new ServiceCollection();
+            services.AddOptions();
+            services.Configure<AzureWebAppManagementOptions>(config);
+            
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetRequiredService<IOptions<AzureWebAppManagementOptions>>();
+            
+            var mgr = new AzureWebAppSslBindingManager(options);
+            await mgr.GetHostNames();
+        }
+    }
+}
