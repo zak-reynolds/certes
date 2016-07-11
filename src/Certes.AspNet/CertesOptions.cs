1e284a80296a525a03b818349b150fbe25d6c4c3
Eddie Lin <fszlin@gmail.com>
2016-07-11 09:04:59 -0400

    Update config api

--- src/Certes.AspNet/CertesOptions.cs
+++ src/Certes.AspNet/CertesOptions.cs
@@ -9,6 +9,5 @@ namespace Certes.AspNet
         public Uri DirectoryUri { get; set; } = WellKnownServers.LetsEncrypt;
         public int MaxSanPerCert { get; set; } = 100;
         public TimeSpan RenewBeforeExpire { get; set; } = TimeSpan.FromDays(30);
-        public List<string> ChallengeTypes { get; set; }
     }
 }
