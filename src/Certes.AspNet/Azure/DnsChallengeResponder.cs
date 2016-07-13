583ad09d9ec120673d5877c2c8e53bc154806ffb
Eddie Lin <fszlin@gmail.com>
2016-07-12 20:17:13 -0400

    Add dns challenge responder.

---
+++ src/Certes.AspNet/Azure/DnsChallengeResponder.cs
@@ -0,0 +1,34 @@
+﻿using Certes.Acme;
+using System;
+using System.Threading.Tasks;
+
+namespace Certes.AspNet.Azure
+{
+    public class DnsChallengeResponder : IChallengeResponder
+    {
+        private readonly IClientCredentialProvider credentialProvider;
+
+        public DnsChallengeResponder(IClientCredentialProvider credentialProvider)
+        {
+            this.credentialProvider = credentialProvider;
+        }
+
+        public string ChallengeType
+        {
+            get
+            {
+                return ChallengeTypes.Dns01;
+            }
+        }
+
+        public Task Deploy(Challenge challenge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(Challenge challenge)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
