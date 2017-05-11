677542018d3e27e6afc18ba5000956e107d5f967
Eddie Lin <fszlin@gmail.com>
2017-05-11 14:40:21 -0400

    update nupkg settings

--- src/Certes.Integration/Properties/AssemblyInfo.cs
+++ src/Certes.Integration/Properties/AssemblyInfo.cs
@@ -1,11 +1,1 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Certes.Azure")]
-[assembly: AssemblyTrademark("")]
-
-[assembly: AssemblyVersion("1.0")]
+﻿
